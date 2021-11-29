5a5b3013df4fb0616018bea1128ed9ee22d49937
luanzera <luan.milano@ambevtech.com.br>
2021-11-29 19:32:49 -0300

    estrutura-inicial

---
+++ src/test/java/progii/locadora/LocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package progii.locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
